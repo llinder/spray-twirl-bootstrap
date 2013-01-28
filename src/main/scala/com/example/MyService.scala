fb1a2c87c4e83132e2c2c29848ba2fae25ad539a
Joost den Boer <jdboer@diversit.eu>
2013-01-28 20:16:09 +0100

    Serving static sources working.

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -26,14 +26,16 @@ class MyServiceActor extends Actor with MyService {
 
 
 // this trait defines our service behavior independently from the service actor
-trait MyService extends HttpService with StaticFilesDirective {
+trait MyService extends HttpService {
 
   def showPath(req: HttpRequest) = LogEntry("Method = %s, Path = %s" format(req.method, req.uri), Logging.InfoLevel)
 
   val myRoute =
     logRequest(showPath _) {
       get {
-        getFromResource("bootstrap") ~
+        path(Rest) { path =>
+          getFromResource("bootstrap/%s" format path)
+        } ~
         path("") {
           respondWithMediaType(`text/html`) {
             // XML is marshalled to `text/xml` by default, so we simply override here
@@ -60,60 +62,4 @@ trait MyService extends HttpService with StaticFilesDirective {
           }
       }
     }
-}
-
-trait StaticFilesDirective {
-  import StaticFilesDirective._
-
-  def withExtensions(extensions: List[String]) = (f: File) => extensions.contains(f.ext)
-
-//  val resourceDirectory = new File(".").find("bootstrap")
-//  println("ResourceDir: "+resourceDirectory)
-
-//  val staticFiles = resourceDirectory.allFiles(withExtensions(List("html", "css", "png","js")))
-//  println("Static files:")
-//  println(staticFiles.mkString("\n"))
-//
-//  import spray.routing.directives.BasicDirectives._
-//
-//  def filename: Directive[String :: HNil] = extract(_.request.uri)
-//
-//  def staticFile: Directive0 = filename.require(staticFiles.contains(_))
-}
-
-object StaticFilesDirective {
-
-  implicit class Dir(val file: File) extends AnyVal {
-    def files: List[File] = if (file.isFile) List(file) else file.listFiles.toList
-
-    def allFiles(includeDirectories: Boolean): List[File] = {
-      def allFilesAcc(acc: List[File], f: File): List[File] = {
-        if (f.isFile) acc :+ f
-        else {
-          var acc2 = acc
-          for {
-            subfile <- f.files
-          } yield acc2 = allFilesAcc(acc2, subfile)
-          acc2
-        }
-      }
-      allFilesAcc(List(), file)
-    }
-
-    def allFiles(p: File => Boolean, includeDirectories: Boolean = false): List[File] = allFiles(includeDirectories).filter(p)
-
-    def find(fn: String): File = allFiles(_.getName == fn).headOption match {
-      case Some(file) => file
-      case None => throw new RuntimeException("Cannot find %s file" format fn)
-    }
-  }
-
-  implicit class FileOps(val file: File) extends AnyVal {
-    def startsWith(prefix: String): Boolean = file.getName.startsWith(prefix)
-
-    def endsWith(suffix: String): Boolean = file.getName.endsWith(suffix)
-
-    def ext: String = file.getName.split('.').last
-  }
-
 }
