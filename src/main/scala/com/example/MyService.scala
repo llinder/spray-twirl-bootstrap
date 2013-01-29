7eb549e5ff8a9948fff02d1e87756db8d0069811
Joost den Boer <jdboer@diversit.eu>
2013-01-29 09:40:56 +0100

    Added links to some example actions/pages on homepage.

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -50,6 +50,12 @@ trait MyService extends HttpService {
                     on
                     <i>spray-can</i>
                     !</h1>
+                  <p>
+                    <h2>Some Twirl and static pages:</h2>
+                    <a href="/index">Simple index page using Twirl</a><br/>
+                    <a href="/index2">Another Twirl page using a Twitter Bootstrap template with dynamic data and using static css and js resources</a><br/>
+                    <a href="/file">Getting content from a static file</a><br/>
+                  </p>
                 </body>
               </html>
             }
